33a6277ee9cdafd47e5c95759ad705f4b96e9ee0
El Mehdi BERCHID <el_mehdi.berchid@nexans.com>
2026-01-15 17:47:32 +0100

    Feature (Up_dating_Counts):  working on new way to up date counting where we deal with streaming unique tokens

--- src/lm_from_scratch/tokenizer/bpe_train_refactor.py
+++ src/lm_from_scratch/tokenizer/bpe_train_refactor.py
@@ -28,31 +28,98 @@ def Unique_Words_Count(chunks: Iterator[str]) -> dict[str:int]:
     return Word_Counts
 
 
-
 def Optimized_Count_pairs(unique_pre_tokens: dict[str:int]):
     Tokens_Count = {}
     Pair_index = {}
     words = []
-    for wid, t in (unique_pre_tokens.keys()):
+    freqs = []
+    for wid, t in enumerate(unique_pre_tokens.keys()):
         e = [bytes([x]) for x in t.encode('utf-8')]
         words.append(e)
+        freqs.append(unique_pre_tokens[t])
         for i in range(len(e)-1):
             pair = (e[i], e[i+1])
             Pair_index.setdefault(pair, []).append((wid, i))
             Tokens_Count[pair] = Tokens_Count.get(pair, 0) + unique_pre_tokens[t]
 
 
-    return Tokens_Count,Pair_index,words
+    return Tokens_Count,Pair_index,words,freqs
 
     
 
+def Up_Dating_Counts(Tokens_Counts,Pair_index,word,pair,freqs):
+    merge = pair[0] + pair[1]
+    occ_by_word = Pair_index[pair] 
+    for (wid,id) in list(occ_by_word):
+        element = word[wid]
+        if (element[id],element[id+1]) == pair:
+            if id> 0 and id< len(element)-2 :
+                
+                    right_merge = element[id-1]
+                    left_merge = element[id+2]
+                    Tokens_Counts[(right_merge,merge)] = Tokens_Counts.get((right_merge,merge),0) + freqs[wid]
+                    Pair_index.setdefault((right_merge,merge),[]).append((wid,id-1))
+
+
+                    Tokens_Counts[(merge,left_merge)] = Tokens_Counts.get((merge,left_merge),0) + freqs[wid]
+                    Pair_index.setdefault((merge,left_merge),[]).append((wid,id))
+                    
+                    
+
+
+
+                    #### NOW WE GONNA DECREASE RIGHT_MERGE,element(i) element, element,left_merge
+
+                    rightpairs = (right_merge,element[id])
+                    leftpairs = (element[id+1],left_merge)
+
+
+
+                    Tokens_Counts[rightpairs] = Tokens_Counts.get(rightpairs,0) - freqs[wid]
+
+                    Pair_index[rightpairs].remove((wid,id-1))
+                
+                    Tokens_Counts[leftpairs] = Tokens_Counts.get(leftpairs,0) - freqs[wid]
+
+                    Pair_index[leftpairs].remove((wid,id+1))
+
+                
+            elif id == 0 :
+                    left_merge = element[id+2]
+                    leftpairs = (element[id+1],left_merge)
+                    Tokens_Counts[(merge,left_merge)] = Tokens_Counts.get((merge,left_merge),0) + freqs[wid]
+                    Pair_index.setdefault((merge,left_merge),[]).append((wid,id))
+                    Tokens_Counts[leftpairs] = Tokens_Counts.get(leftpairs,0) - freqs[wid]
+                    Pair_index[leftpairs].remove((wid,id+1))
+
+
+
+
+            elif id == len(element)-2:
+                    right_merge = element[id - 1]
+                    rightpairs = (right_merge,element[id])
+                    Tokens_Counts[(right_merge,merge)] = Tokens_Counts.get((right_merge,merge),0) + freqs[wid]
+                    Pair_index.setdefault((right_merge,merge),[]).append((wid,id-1))
+                    Tokens_Counts[rightpairs] = Tokens_Counts.get(rightpairs,0) - freqs[wid]
+                    Pair_index[rightpairs].remove((wid,id-1))
+    Tokens_Counts.pop(pair)
+    Pair_index[pair].clear()
 
 
 
 
 
+        
+
+
+
+
+
+
+
+
+                
             
-         
 
 
 
@@ -69,3 +136,6 @@ def Optimized_Count_pairs(unique_pre_tokens: dict[str:int]):
 
 
 
+
+        
+            
